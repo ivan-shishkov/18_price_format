39858f0448a9b991a2abe4a824f2a3b0d2260b00
Ivan Shishkov <mail-shishkov@yandex.ru>
2018-06-20 19:38:46 +0300

    Added test when price has incorrect string value

--- tests.py
+++ tests.py
@@ -1,1 +1,12 @@
-# TODO place your tests here
+import unittest
+
+from format_price import format_price
+
+
+class FormatPriceTest(unittest.TestCase):
+    def test_incorrect_string_value_is_not_price_value(self):
+        self.assertIsNone(format_price(price='price'))
+
+
+if __name__ == '__main__':
+    unittest.main()
