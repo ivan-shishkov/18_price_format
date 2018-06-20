6f82add88e61b27c35cba6cf99bbf4a0a68cf19f
Ivan Shishkov <mail-shishkov@yandex.ru>
2018-06-20 19:53:31 +0300

    Added tests for check whether built-in types (bool, list, set, dict) are incorrect price values

--- tests.py
+++ tests.py
@@ -10,8 +10,24 @@ class FormatPriceTest(unittest.TestCase):
     def test_infinity_is_incorrect_price_value(self):
         self.assertIsNone(format_price(price='Infinity'))
 
-    def test_nan_is_incorrect_price_value_price(self):
+    def test_nan_is_incorrect_price_value(self):
         self.assertIsNone(format_price(price='nan'))
+
+    def test_bool_is_incorrect_price_value(self):
+        self.assertIsNone(format_price(price=True))
+        self.assertIsNone(format_price(price=False))
+
+    def test_list_is_incorrect_price_value(self):
+        self.assertIsNone(format_price(price=[]))
+        self.assertIsNone(format_price(price=['1234']))
+
+    def test_set_is_incorrect_price_value(self):
+        self.assertIsNone(format_price(price=set()))
+        self.assertIsNone(format_price(price={1234}))
+
+    def test_dict_is_incorrect_price_value(self):
+        self.assertIsNone(format_price(price={}))
+        self.assertIsNone(format_price(price={'price': 1234}))
 
 
 if __name__ == '__main__':
