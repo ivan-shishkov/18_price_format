bbc79914e78daf4a66d58c02103344eebba93175
Ivan Shishkov <mail-shishkov@yandex.ru>
2018-06-20 15:33:25 +0300

    Added parsing of command line arguments

--- format_price.py
+++ format_price.py
@@ -1,9 +1,31 @@
-def format_price(price):
-    pass
+import argparse
+
+
+def parse_command_line_arguments():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        'price',
+        help='a price for pretty formatting',
+        type=str,
+    )
+    parser.add_argument(
+        '--digits',
+        help='a count of digits precision after decimal point for rounding '
+             'of price value if it is not a integer value (default: 2)',
+        default=2,
+        type=int,
+    )
+    command_line_arguments = parser.parse_args()
+
+    return command_line_arguments
 
 
 def main():
-    pass
+    command_line_arguments = parse_command_line_arguments()
+
+    price = command_line_arguments.price
+    count_digits_after_point = command_line_arguments.digits
 
 
 if __name__ == '__main__':
