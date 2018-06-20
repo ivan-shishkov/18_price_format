c894b2888d883d2ae5a37a8cba358c2e8cf09ec7
Ivan Shishkov <mail-shishkov@yandex.ru>
2018-06-20 20:19:24 +0300

    Added test for both positive and negative values (integer and float)

--- tests.py
+++ tests.py
@@ -29,6 +29,32 @@ class FormatPriceTest(unittest.TestCase):
         self.assertIsNone(format_price(price={}))
         self.assertIsNone(format_price(price={'price': 1234}))
 
+    def test_price_format_for_positive_integer_value(self):
+        self.assertEqual('1 234', format_price(price='1234'))
+        self.assertEqual('1 234', format_price(price='1234.0000'))
+        self.assertEqual('1 234', format_price(price=1234))
+        self.assertEqual('1 234', format_price(price=1234.0))
+
+    def test_price_format_for_negative_integer_value(self):
+        self.assertEqual('-4 321', format_price(price='-4321'))
+        self.assertEqual('-4 321', format_price(price='-4321.0000'))
+        self.assertEqual('-4 321', format_price(price=-4321))
+        self.assertEqual('-4 321', format_price(price=-4321.0))
+
+    def test_price_format_for_zero_value(self):
+        self.assertEqual('0', format_price(price='0'))
+        self.assertEqual('0', format_price(price='000.0000'))
+        self.assertEqual('0', format_price(price=0))
+        self.assertEqual('0', format_price(price=0.0))
+
+    def test_price_format_for_positive_float_value(self):
+        self.assertEqual('1 234.56', format_price(price='1234.56'))
+        self.assertEqual('1 234.56', format_price(price=1234.56))
+
+    def test_price_format_for_negative_float_value(self):
+        self.assertEqual('-1 234.56', format_price(price='-1234.56'))
+        self.assertEqual('-1 234.56', format_price(price=-1234.56))
+
 
 if __name__ == '__main__':
     unittest.main()
