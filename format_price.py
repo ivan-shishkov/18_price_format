5debcc5d0d2872a7f28a69bfabfad565b500aaef
Ivan Shishkov <mail-shishkov@yandex.ru>
2018-06-20 16:03:05 +0300

    Added function for format price

--- format_price.py
+++ format_price.py
@@ -1,4 +1,29 @@
 import argparse
+import math
+
+
+def format_price(price, count_digits_after_point=2):
+    if isinstance(price, bool):
+        return None
+
+    try:
+        price_float_value = float(price)
+    except ValueError:
+        return None
+
+    if price_float_value is None:
+        return None
+
+    if not math.isfinite(price_float_value):
+        return None
+
+    if price_float_value.is_integer():
+        return '{:,.0f}'.format(price_float_value).replace(',', ' ')
+    else:
+        return '{:,.{precision}f}'.format(
+            price_float_value,
+            precision=count_digits_after_point,
+        ).replace(',', ' ')
 
 
 def parse_command_line_arguments():
