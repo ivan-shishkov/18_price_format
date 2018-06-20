ba7da6ea9434f9658ef0d180a9013dcbd0cb1012
Ivan Shishkov <mail-shishkov@yandex.ru>
2018-06-20 19:42:25 +0300

    Added tests for check whether special float values "infinity" and "NaN" is incorrect price values

--- tests.py
+++ tests.py
@@ -7,6 +7,12 @@ class FormatPriceTest(unittest.TestCase):
     def test_incorrect_string_value_is_not_price_value(self):
         self.assertIsNone(format_price(price='price'))
 
+    def test_infinity_is_incorrect_price_value(self):
+        self.assertIsNone(format_price(price='Infinity'))
+
+    def test_nan_is_incorrect_price_value_price(self):
+        self.assertIsNone(format_price(price='nan'))
+
 
 if __name__ == '__main__':
     unittest.main()
